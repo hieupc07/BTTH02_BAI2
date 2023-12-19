4a7099bf9cbf32c6eea9c506c9d6c191418b2508
hieupt07 <93933039+hieupc07@users.noreply.github.com>
2023-12-20 01:00:13 +0700

    BTTH3_B2

---
+++ INDEX/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'config.php';
+$controller = isset($_GET["controller"]) ? $_GET["controller"] : 'questions';
+$action = isset($_GET["action"]) ? $_GET["action"] : 'edit';
+
+$controllerClass = ucfirst($controller) . "Controller";
+
+$controllerFile = "controller/$controllerClass.php";
+
+if(file_exists($controllerFile)){
+    require_once "./".$controllerFile;
+    $controllerInstance  = new $controllerClass();
+    $controllerInstance->$action();
+}else {
+    echo "Controller not found " . $controllerFile;
+}
+?>
